b30e2ff20fe811d49b61227503972efea2c0a9ec
Victoria <131689116+7slivka7@users.noreply.github.com>
2024-02-11 15:10:10 +0200

    adding new files

--- script.js
+++ script.js
@@ -4,12 +4,21 @@ const btnNo = document.querySelector('.no');
 let btnYesFz = 2;
 let btnNoFz = 2;
 
-btnNo.addEventListener('click', doYesBiger);
+btnNo.addEventListener('click', function () {
+    doYesBiger();
+});
 
+btnYes.addEventListener('click', function () {
+    buttonYesPushed();
+});
 
 function doYesBiger(){
     btnYesFz += .4;
     btnYes.style.fontSize = btnYesFz + 'em';
     btnNoFz -=.5;
     btnNo.style.fontSize = btnNoFz + 'em';
+}
+
+function buttonYesPushed (){
+    location.href = './page2.html';
 }
